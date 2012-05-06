221e6a5c9cfeffe1908bea4a572950e4b8682488
piercelover <piercelover@gmail.com>
2012-05-06 13:33:31 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moo::Application.config.secret_token = '187d7c35ece5331df64c65c7ade3221aa908b9754ce581c0a7b9036cf9148bea8aa5f207b6d483a7c34fa2d77eb357ed7b7a76c9076be4a01b7f4e7b10eda903'
